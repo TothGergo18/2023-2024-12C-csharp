2ea9f85da8011e226555b0973fa2a13a1d087e55
2020092 <2020092@B109-05>
2023-09-08 14:24:55 +0200

    Add project files.

---
+++ teszt/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teszt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Későbbi fejlesztés
+            Console.WriteLine("Hello teszt");
+            Console.ReadKey();
+        }
+    }
+}
